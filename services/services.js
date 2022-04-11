3869949c5707f38685eb100a0a616956267b23e3
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-11 13:39:40 +0300

    Update services.js

--- services/services.js
+++ services/services.js
@@ -1,19 +1,18 @@
-let usersData = require("../data");
-
 class userService {
-
-    users = usersData;
+    User = require("../models/model");
+    users = this.User.findAll({attributes: ['id', 'name']}).then(data => this.users = data).catch(err => console.log(err));
 
     getAll = () => {
-        return this.users;
+        return this.users
     }
 
     getById = (key) => {
-        return this.users.filter(user => key === user.id);
+        return this.users.filter(user => key == user.id);
     }
 
     deleteById = (key) => {
         this.users = this.users.filter(user => key != user.id);
+        this.User.destroy({ where: { id: key } }).then((res) => console.log(res));
         return this.users;
     }
 
@@ -22,17 +21,21 @@ class userService {
         this.users.forEach(elem => {
             uid = Math.max(elem.id);
         })
-        this.users.push({ name: data, id: ++uid });
+        ++uid;
+        this.users.push({ id: Number(uid), name: data });
+        this.User.create({ name: data }).then(res => console.log(res)).catch(err => console.log(err));
         return this.users;
     }
 
     update = (data, key) => {
+        this.User.update({ name: data }, { where: { id: key } }).then(res => console.log(res)).catch(err => console.log(err));
         this.users = this.users.filter(user => {
-            if (key === user.id) {
+            if (key == user.id) {
                 user.name = data;
             }
             return user;
         })
+
         return this.users;
     }
 

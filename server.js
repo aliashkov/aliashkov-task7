e0ea34aff831ad0bad982dd6a07f3f6f15ece7e3
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-11 13:39:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const usersRouter = require('./router/router')
+const usersRouter = require('./router/router');
 
 const app = express();
 const port = 3000;
+
+const sequelize = require("./connection/connection");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', usersRouter)
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+
+sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
 })

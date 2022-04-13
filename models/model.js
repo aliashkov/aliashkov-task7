78cca3cee23c22555c79a7101fe88a22865df41f
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-13 14:06:16 +0300

    Update model.js

--- models/model.js
+++ models/model.js
@@ -11,6 +11,10 @@ const User = sequelize.define("user", {
     name: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 });
 

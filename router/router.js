02e1ac8dcbbc8debde13aaf586b8e080b05e6cd4
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-13 14:05:25 +0300

    Update router.js

--- router/router.js
+++ router/router.js
@@ -2,12 +2,14 @@ const express = require("express");
 const userController = require("../controller/controller");
 
 const usersRouter = express.Router();
+const auth = require('../middleware/auth');
 
 usersRouter
     .get('/', userController.getAll)
     .get('/:id', userController.getById)
-    .post('/', userController.add)
-    .put('/:id', userController.update)
-    .delete('/:id', userController.deleteById)
+    .post('/login', userController.login)
+    .post('/',  userController.add)
+    .put('/:id', auth, userController.update)
+    .delete('/:id', auth, userController.deleteById)
 
 module.exports = usersRouter;

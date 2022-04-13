64496d014c2f83420fde11d19545a47f66a79c7c
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-13 14:05:04 +0300

    Update services.js

--- services/services.js
+++ services/services.js
@@ -1,59 +1,26 @@
-class userService {
-    User = require("../models/model");
-    users = this.User.findAll({ attributes: ['id', 'name'] }).then(data => this.users = data).catch(err => console.log(err));
+const User = require('../models/model')
 
-    getAll = () => {
-        return this.users
+class UserService {
+
+    getUsers() {
+        return User.findAll()
     }
 
-    getById = (key) => {
-        const foundKey = this.users.find(user => key == user.id);
-        if (foundKey != null)
-            return foundKey
-        return "No such key in this table"
+    getUserById(id) {
+        return User.findOne({ where: { id } })
     }
 
-    deleteById = (key) => {
-        const foundKey = this.users.find(user => key == user.id);
-        if (foundKey != null) {
-            this.User.destroy({ where: { id: key } }).then((res) => console.log(res));
-            return this.users;
-        }
-        return "No such key in this table"
+    addUser(newUser) {
+        return User.create(newUser)
     }
 
-    add = (data) => {
-        const foundValue = this.users.find(user => user.name == data);
-        if (foundValue == null) {
-            let uid = 0;
-            this.users.forEach(elem => {
-                uid = Math.max(elem.id);
-            })
-            ++uid;
-            this.users.push({ id: Number(uid), name: data });
-            this.User.create({ name: data }).then(res => console.log(res)).catch(err => console.log(err));
-            return this.users;
-        }
-        return "This value already exists in database"
-
+    changeUser(id, { name, password }) {
+        return User.update({ name, password }, { where: { id } })
     }
 
-    update = (data, key) => {
-        const foundKey = this.users.find(user => key == user.id);
-        if (foundKey != null) {
-            this.User.update({ name: data }, { where: { id: key } }).then(res => console.log(res)).catch(err => console.log(err));
-            this.users = this.users.filter(user => {
-                if (key == user.id) {
-                    user.name = data;
-                }
-                return user;
-            })
-            return this.users;
-        }
-        return "No such key in this table"
-
+    deleteUser(id) {
+        return User.destroy({ where: { id } })
     }
-
 }
 
-module.exports = new userService();
+module.exports = new UserService();

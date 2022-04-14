8db00ff4ac4143c6feeb564b416b10555fe58084
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-14 11:47:33 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
-const config = require("../config")
+const config = require("../config");
+const User = require('../models/model');
+const service = require('../services/services');
 
-const auth = (req,res,next)=>{
 
-    try{
-
-        const [strategy,token] = req.headers['authorization'].split(' ');
+const auth = async (req, res, next) => {
+    try {
+        const [strategy, token] = req.headers['authorization'].split(' ');
         console.log(strategy);
         console.log(token);
         const result = jwt.verify(token, config.secret);
-        req.login = result.login;
-        next();
-
+        const users = await service.getUsers();
+        const foundValue = service.findUser(users,  result.login);
+        if (foundValue != null) {
+            req.login = result.login;
+            next();
+        } else {
+            return res.status(400).json({ message: 'This user doesnt exists' });
+        }
     } catch (e) {
-
         res.status(401).send(e.message);
     }
 
 }
 
-
 module.exports = auth;
+

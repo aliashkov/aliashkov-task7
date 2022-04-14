2d4733cfdc4ffcf5ea0e5267dad430570351c307
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-14 11:45:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const usersRouter = require('./router/router');
-
+const unauthRouter = require("./router/unauthRouter");
+const authRouter = require("./router/authRouter");
 const app = express();
 const port = 3000;
 
 const sequelize = require("./connection/connection");
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/users', usersRouter)
-
+app.use('/users', unauthRouter);
+app.use('/users', authRouter);
 
 sequelize.sync().then(() => {
     app.listen(port, () => {

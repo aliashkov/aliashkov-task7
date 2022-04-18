00f4766b761814ff88dc1b0acfdc26a5bc05801c
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-18 11:28:57 +0300

    Update check.js

--- middleware/check.js
+++ middleware/check.js
@@ -2,9 +2,14 @@ const {check} = require('express-validator')
 
 const loginValidator = () => {
     return [
-        check('name', "User name cannot null").notEmpty(),
-        check('password', "Password length less than 3 symbols").isLength({min : 3})
+        check('name', "User name cannot null")
+           .notEmpty()
+           .matches(/^[A-Za-z]+$/),
+        check('password', "Password should contain at least 3 symbols")
+           .isLength({min : 3})
+           .matches(/^[A-Za-z0-9]+$/)
     ]
 }
 
 module.exports = loginValidator;
+

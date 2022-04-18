935f7c7cf5146d674e06f3843afea160b75cc66f
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-18 11:32:46 +0300

    Update unauthRouter.js

--- router/unauthRouter.js
+++ router/unauthRouter.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const userController = require("../controller/controller");
-const loginValidator = require('../middleware/check')
+const loginValidator = require('../middleware/check');
+const multer = require('../middleware/multer')
 
 const unauthRouter = express.Router();
+
 
 unauthRouter
     .get('/', userController.getAll)
     .get('/:id', userController.getById)
     .post('/login', userController.login)
-    .post('/', loginValidator(), userController.add)
+    .post('/',  multer, loginValidator(),  userController.add)
 
 module.exports = unauthRouter;

0b394df25ef2dc0a87a8a6fed7788312e8925968
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-18 11:32:33 +0300

    Update authRouter.js

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const userController = require("../controller/controller");
+const loginValidator = require('../middleware/check')
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer')
 
 const authRouter = express.Router();
-const auth = require('../middleware/auth');
+
 
 authRouter
-    .put('/:id', auth, userController.update)
+    .put('/:id', auth, multer, loginValidator(),  userController.update)
     .delete('/:id', auth, userController.deleteById)
 
 module.exports = authRouter;

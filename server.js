32417fd14f740548b67b577a832665658cf4b34c
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-19 11:38:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const authRouter = require("./router/authRouter");
 const app = express();
 const port = 3000;
 
-const sequelize = require("./connection/connection");
-
+const connect = require("./connection/connection");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', unauthRouter);
 app.use('/users', authRouter);
 
-sequelize.sync().then(() => {
+connect.then(() =>{
     app.listen(port, () => {
         console.log(`Server listening on port ${port}`)
     })

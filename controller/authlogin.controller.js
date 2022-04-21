c189d3bf3f4f112c53bd9f2edcdf60240826aaf7
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-21 10:01:24 +0300

    Update and rename controller.js to authlogin.controller.js

--- controller/authlogin.controller.js
+++ controller/authlogin.controller.js
@@ -1,5 +1,24 @@
-const service = require('../services/services');
+const serviceUsers = require('../services/user.services');
+const serviceAuth = require('../services/authlogin.controller');
 const { validationResult } = require('express-validator')
+
+
+const add = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(400).json({ message: errors });
+        const { userName, hash } = serviceAuth.getNameAndEncryptedPassword(req.body.name, req.body.password);
+        const users = await serviceUsers.getUsers();
+        const foundValue = serviceUsers.findUser(users, userName);
+        if (foundValue != null)
+            return res.status(400).json({ message: 'This user already exists' });
+        const result = await serviceUsers.addUser({ name: userName, password: hash, photo: req.file.path });
+        res.json(result);
+    } catch (err) {
+        return res.status(400).json({ message: `Database error - ${err}` });
+    }
+}
 
 const login = async (req, res) => {
     try {
@@ -7,12 +26,12 @@ const login = async (req, res) => {
         if (!errors.isEmpty())
             return res.status(400).json({ message: errors });
         const [login, password] = [req.body.name, req.body.password];
-        const users = await service.getUsers();
-        const foundValue = service.findUser(users, login);
+        const users = await serviceUsers.getUsers();
+        const foundValue = serviceUsers.findUser(users, login);
         if (foundValue != null) {
-            const verifyPasswordAndLogin = service.verifyPasswordAndLogin(foundValue, login, password);
+            const verifyPasswordAndLogin = serviceAuth.verifyPasswordAndLogin(foundValue, login, password);
             if (verifyPasswordAndLogin) {
-                const token = service.getToken(login, foundValue.password);
+                const token = serviceAuth.getToken(login, foundValue.password);
                 return res.send(token);
             }
             return res.status(400).json({ message: 'Incorrect login or password' });
@@ -24,15 +43,10 @@ const login = async (req, res) => {
     }
 }
 
-const getAll = async (req, res) => {
-    try {
-        const users = await service.getUsers();
-        res.send(users);
-    } catch (err) {
-        return res.status(400).json({ message: 'Database error' });
-    }
+module.exports = {
+    login,
+    add
 }
-
 const getById = async (req, res) => {
     try {
         const result = await service.getUserById(req.params.id)

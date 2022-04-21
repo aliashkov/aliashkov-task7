2874f298916212797528c92af9cd6a81d74e7f3a
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-21 10:05:52 +0300

    Create authlogin.controller.js

---
+++ services/authlogin.controller.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const config = require("../config");
+const jwt = require('jsonwebtoken');
+
+class AuthService {
+
+    getNameAndEncryptedPassword(user, password) {
+        const userName = user;
+        const hash = bcrypt.hashSync(password, config.key);
+        return { userName, hash };
+    }
+
+    verifyPasswordAndLogin(foundUser, user, password) {
+        const verifyPassword = bcrypt.compareSync(password, foundUser.password);
+        if ((user === foundUser.name) && (verifyPassword === true))
+            return true;
+        return false;
+    }
+
+    getToken(userName, passwordValue) {
+        return jwt.sign({ login: userName, password: passwordValue }, config.secret);
+    }
+}
+
+module.exports = new AuthService();
